b49ee6ae27542d450fc83fe1fc04a39a83d6c291
railsbug <railsbug@126.com>
2017-12-01 18:52:21 +0800

    第二次提交

---
+++ html/files/录入/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(v,w,x,y),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,L,M,D,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="录入.html",d="generationDate",e=new Date(1512119156137.31),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="0aeac6f862a8475a87cad74184bf2b49",q="type",r="Axure:Page",s="name",t="录入",u="notes",v="1",w="<p><span>11</span></p>",x="2",y="<p><span>22</span></p>",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFF333333,J="image",K="imageHorizontalAlignment",L="center",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
